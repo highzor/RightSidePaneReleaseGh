ee1b51606e2c7a852923edc5ede231b1ebfb5fcc
highzor <52636901+highzor@users.noreply.github.com>
2020-12-31 16:20:29 +0300

    Update CallerHepler.cs

--- WServiceCRM/ServiceCRM/Helpers/CallerHepler.cs
+++ WServiceCRM/ServiceCRM/Helpers/CallerHepler.cs
@@ -3,7 +3,7 @@ namespace ServiceCRM.Helpers
 {
     public class CallerHepler
     {
-        public int Code { get; set; }
+        public int? Code { get; set; }
         public string ErrorMessage { get; set; }
         public string FullName { get; set; }
         public string DateOfBirth { get; set; }
