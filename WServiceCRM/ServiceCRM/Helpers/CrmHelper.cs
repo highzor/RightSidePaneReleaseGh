6892031db88da61671ee4f5b83b92b00dc4d2aca
highzor <52636901+highzor@users.noreply.github.com>
2020-12-31 15:00:19 +0300

    Update CrmHelper.cs

--- WServiceCRM/ServiceCRM/Helpers/CrmHelper.cs
+++ WServiceCRM/ServiceCRM/Helpers/CrmHelper.cs
@@ -230,9 +230,9 @@ namespace ServiceCRM.Helpers
         {
             IOrganizationService service = null;
             ClientCredentials clientCredentials = new ClientCredentials();
-            clientCredentials.UserName.UserName = "apetukhov";
-            clientCredentials.UserName.Password = "12Qwerty";
-            service = (IOrganizationService)new OrganizationServiceProxy(new Uri("http://10.40.10.146/LearnAPetukhov/XRMServices/2011/Organization.svc"),
+            clientCredentials.UserName.UserName = "username";
+            clientCredentials.UserName.Password = "password";
+            service = (IOrganizationService)new OrganizationServiceProxy(new Uri("http://XX.XX.XX.XXX/LearnAPetukhov/XRMServices/2011/Organization.svc"),
              null, clientCredentials, null);
             return service;
         }
